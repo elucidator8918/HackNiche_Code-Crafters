bbc75f12a4c4d95539770abd3346ce492df1f050
Hetens <92987462+Hetens@users.noreply.github.com>
2023-02-18 02:51:43 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Signup from './Signup';
 import './Signup.css';
 import Slider from './components/Slider';
+import StockDashboard from './components/StockDashboard';
+
+
 
 function App() {
   return (
@@ -10,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Signup/>}/>
+          <Route path='/StockDashboard' element={<StockDashboard/>}/>
           <Route path='/slider' element={<Slider/>}/>
         </Routes>
       

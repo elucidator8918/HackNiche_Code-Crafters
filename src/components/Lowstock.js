92728c5a257770a1cdb6a2f808f351f74c5ba322
Hetens <92987462+Hetens@users.noreply.github.com>
2023-02-18 03:17:55 +0530

    Working Model

---
+++ src/components/Lowstock.js
@@ -0,0 +1,97 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+export default function Lowrisk(){
+    const LowRiskButton = () => {
+        const [showNav, setShowNav] = useState(false);
+        const [showInfosys, setShowInfosys] = useState(false);
+        const [showkotak, setShowkotak] = useState(false);
+        const [showHDFC, setShowHDFC] = useState(false);
+        const [showHCLTECH, setShowHCLTECH] = useState(false);
+        const [duration,setDuration] =useState('')
+        const [money,setMoney] =useState(0)
+        const[Amt,setAmt] = useState(0)
+        const handleInfosysClick =()=>{
+          setShowInfosys(true)
+        }
+        const handlekotakClick =()=>{
+          setShowkotak(true)
+        }
+        const handleHDFCClick =()=>{
+          setShowHDFC(true)
+        }
+        const handleHCLTECHClick =()=>{
+          setShowHCLTECH(true)
+        }
+        const handleButtonClick = () => {
+          setShowNav(true);
+        }
+        const handleDuration =(e) =>{
+          setDuration(e.target.value)
+        }
+        const handleMoney =(e) =>{
+          setMoney(e.target.value)
+        }
+        const handleSubmit = async(event) => {
+          event.preventDefault()
+        const formData = new FormData();
+          formData.append("compony", "Infosys");
+          formData.append("duration",duration);
+         
+          try {
+            const response = await axios.post('http://127.0.0.1:8000/model/model-set/', formData, {
+              headers: { 'Content-Type': 'multipart/form-data' },
+            });
+            console.log(response);
+            const stockAmount = response.data.pred * money;
+            console.log(stockAmount)
+            setAmt(stockAmount);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        return (
+          <div>
+            {!showNav && <button onClick={handleButtonClick}>Low risk</button>}
+            {showNav && (
+              <nav style={{display:"flex"}}>
+                <ul>
+                  
+                  <button onClick ={handleInfosysClick}>Infosys</button>
+                  {showInfosys&&(<form>
+                      
+                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
+                      <input type='text' placeholder='Money' onChange={handleMoney}></input>
+                      <button type = 'submit' onClick={handleSubmit}> submit</button>
+                  </form>)}
+                  <button onClick={handlekotakClick}>kotak</button>
+                  {showkotak&&(<form>
+                      
+                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
+                      <input type='text' placeholder='Money'onChange={handleMoney}></input>
+                      <button type = 'submit'> submit</button>
+                  </form>)}
+                  <button onClick={handleHDFCClick}>HDFC</button>
+                  {showHDFC&&(<form>
+                      
+                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
+                      <input type='text' placeholder='Money' onChange={handleMoney}></input>
+                      <button type = 'submit'> submit</button>
+                  </form>)}
+                  <button onClick={handleHCLTECHClick}>HCLTECH</button>
+                  {showHCLTECH&&(<form>
+                      
+                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
+                      <input type='text' placeholder='Money' onChange={handleMoney}></input>
+                      <button type = 'submit'> submit</button>
+                  </form>)}
+                  <h1>{Amt}</h1>
+                </ul>
+              </nav>
+            )}
+          </div>
+        );
+      }
+      return(
+        <LowRiskButton/>
+      )
+}
--- src/components/StockDashboard.js
+++ src/components/StockDashboard.js
@@ -1,97 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import Lowstock from './Lowstock'
 
 
-function Lowrisk(){
-    const LowRiskButton = () => {
-        const [showNav, setShowNav] = useState(false);
-        const [showBajaj, setShowBajaj] = useState(false);
-        const [showHindustan, setShowHindustan] = useState(false);
-        const [showCipla, setShowCipla] = useState(false);
-        const [showAbbott, setShowAbbott] = useState(false);
-        const [duration,setDuration] =useState('')
-        const [money,setMoney] =useState(0)
-        const[Amt,setAmt] = useState(0)
-        const handleBajajClick =()=>{
-          setShowBajaj(true)
-        }
-        const handleHindustanClick =()=>{
-          setShowHindustan(true)
-        }
-        const handleCiplaClick =()=>{
-          setShowCipla(true)
-        }
-        const handleAbbottClick =()=>{
-          setShowAbbott(true)
-        }
-        const handleButtonClick = () => {
-          setShowNav(true);
-        }
-        const handleDuration =(e) =>{
-          setDuration(e.target.value)
-        }
-        const handleMoney =(e) =>{
-          setMoney(e.target.value)
-        }
-        const handleSubmit = async(event) => {
-          event.preventDefault()
-        const formData = new FormData();
-          formData.append("compony", "Bajaj");
-          formData.append("duration",duration);
-         
-          try {
-              axios.post( "http://127.0.0.1:8000/model/model-set/ ",formData,
-              {headers:{"Content-Type": "multipart/form-data"} }).then(res=>{console.log(res.data.pred)
-          setAmt((res.data.pred*money).value)
-          console.log(Amt);
-              }).catch(err =>{console.log(err)});
-             } catch(error) {
-               console.log(error)
-             }
-        }
-        return (
-          <div>
-            {!showNav && <button onClick={handleButtonClick}>High Risk</button>}
-            {showNav && (
-              <nav style={{display:"flex"}}>
-                <ul>
-                  
-                  <button onClick ={handleBajajClick}>Bajaj</button>
-                  {showBajaj&&(<form>
-                      
-                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
-                      <input type='text' placeholder='Money'></input>
-                      <button type = 'submit' onClick={handleSubmit}> submit</button>
-                  </form>)}
-                  <button onClick={handleHindustanClick}>Hindustan</button>
-                  {showHindustan&&(<form>
-                      
-                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
-                      <input type='text' placeholder='Money'onChange={handleMoney}></input>
-                      <button type = 'submit'> submit</button>
-                  </form>)}
-                  <button onClick={handleCiplaClick}>Cipla</button>
-                  {showCipla&&(<form>
-                      
-                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
-                      <input type='text' placeholder='Money' onChange={handleMoney}></input>
-                      <button type = 'submit'> submit</button>
-                  </form>)}
-                  <button onClick={handleAbbottClick}>ABBOTT</button>
-                  {showAbbott&&(<form>
-                      
-                      <input type='text' placeholder='Duration' onChange={handleDuration}></input>
-                      <input type='text' placeholder='Money' onChange={handleMoney}></input>
-                      <button type = 'submit'> submit</button>
-                  </form>)}
-                  <h1>{Amt}</h1>
-                </ul>
-              </nav>
-            )}
-          </div>
-        );
-      }
-}
+
 export default function StockDashboard(){
     // const [selectedOptions, setSelectedOptions] = useState([]);
     // const dashboardStyles = {
@@ -273,6 +185,8 @@ const HighRiskButton = () => {
 
 
     return(
-        <HighRiskButton/>
+        <div>
+        <HighRiskButton/><Lowstock/>
+        </div>
     )
 }
